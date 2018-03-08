15ac0ab906703e15be721eaa2e2fe6bf8dc80d2d
James Reynolds <james.reynolds@cristiesoftware.com>
2018-03-08 09:34:31 +0000

    Change to use a condition variable

--- gcovr/workers.py
+++ gcovr/workers.py
@@ -7,7 +7,7 @@
 # This software is distributed under the BSD license.
 
 
-from threading import Thread, Lock
+from threading import Thread, Condition
 from contextlib import contextmanager
 
 import sys
@@ -22,29 +22,27 @@ class LockedDirectories(object):
     Class that keeps a list of locked directories
     """
     def __init__(self):
-        self.lock = Lock()
         self.dirs = set()
+        self.cv = Condition()
 
     def run_in(self, dir_):
         """
         Start running in the directory and lock it
         """
-        self.lock.acquire()
-        if dir_ not in self.dirs:
-            self.dirs.add(dir_)
-            self.lock.release()
-            return
-        self.lock.release()
-        import time
-        time.sleep(0.1)
-        return self.run_in(dir_)
+        self.cv.acquire()
+        while dir_ in self.dirs:
+            self.cv.wait()
+        self.dirs.add(dir_)
+        self.cv.release()
 
     def done(self, dir_):
         """
         Finished with the directory, unlock it
         """
-        with self.lock:
-            self.dirs.remove(dir_)
+        self.cv.acquire()
+        self.dirs.remove(dir_)
+        self.cv.notify()
+        self.cv.release()
 
 
 @contextmanager
